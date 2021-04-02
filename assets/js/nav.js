f3c64476520b7cd48ae15b1ef6ab7f0d55e67a21
muxima2010 <81822597+muxima2010@users.noreply.github.com>
2021-04-02 18:16:15 +0100

    Tuca Site

---
+++ assets/js/nav.js
@@ -0,0 +1,11 @@
+//JS File
+
+//Btn Menu for mobile
+
+const menu = document.querySelector("#mobile-menu")
+const menuLinks = document.querySelector(".nav-menu")
+
+menu.addEventListener("click", function(){
+    menu.classList.toggle("is-active");
+    menuLinks.classList.toggle("active");
+})
